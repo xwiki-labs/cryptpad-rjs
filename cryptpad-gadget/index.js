62b93b2babae240ab50f8dfce384da079195e015
Fabio Mancinelli <fabio.mancinelli@xwiki.com>
2015-01-30 11:00:05 +0100

    Add UI and navigation logic

--- cryptpad-gadget/index.js
+++ cryptpad-gadget/index.js
@@ -1,18 +1,24 @@
 (function (window, rJS, require) {
-  /* Gadget definition */
-  rJS(window).ready(function (g) {
-    /* We use the explicit requirejs module loading because data-main script attribute in the HTML
-     is not taken into account when the gadget is loaded via RenderJS */
-    require.config({
-      baseUrl: '../lib/cryptpad/www/'
+    var cryptPadLoadedCallback;
+
+    /* Gadget definition */
+    rJS(window).ready(function (g) {
+        /* We use the explicit requirejs module loading because data-main script attribute in the HTML
+         is not taken into account when the gadget is loaded via RenderJS */
+        require.config({
+            baseUrl: '../lib/cryptpad/www/'
+        });
+
+        require(['main'], function (main) {
+            var cryptPadURL = 'http://localhost:3000/api/config?cb=' + Math.random().toString(16).substring(2)
+
+            console.log("Starting up cryptpad with relay: " + cryptPadURL);
+
+            main('editor1', cryptPadURL, cryptPadLoadedCallback);
+        });
     });
 
-    require(['main'], function (main) {
-      var cryptpadURL = 'http://localhost:3000/api/config?cb=' + Math.random().toString(16).substring(2)
-
-      console.log("Starting up cryptpad with relay: " + cryptpadURL);
-
-      main(cryptpadURL);
+    rJS(window).declareMethod("setCryptPadLoadedCallback", function (callback) {
+        cryptPadLoadedCallback = callback;
     });
-  });
 })(window, rJS, require);
--- cryptpad/index.js
+++ cryptpad/index.js
@@ -1,11 +1,24 @@
 (function (window, rJS) {
-    rJS(window).ready(function (g) {
-        g.render();
-    }).declareMethod("render", function () {
-        var gadget = this;
+    "use strict";
 
-        return RSVP.all([
-            gadget.declareGadget("../cryptpad-gadget/index.html", {element: gadget.__element})
-        ]);
+    $.mobile.ajaxEnabled = false;
+    $.mobile.linkBindingEnabled = false;
+    $.mobile.hashListeningEnabled = false;
+    $.mobile.pushStateEnabled = false;
+
+    rJS(window).declareMethod("render", function (options) {
+        var thisGadget = this;
+
+        var pageElement = thisGadget.__element.getElementsByClassName('page')[0];
+        pageElement.innerHTML = '';
+
+        switch (options.page) {
+            case 'cryptpad':
+                return thisGadget.declareGadget('../cryptpad-gadget/index.html', {element: pageElement}).then(function (cryptPadGadget) {
+                    return cryptPadGadget.setCryptPadLoadedCallback(function (cryptPad) {
+                        thisGadget.__element.getElementsByClassName('title')[0].innerHTML = 'CryptPAD: ' + cryptPad.getKey();
+                    });
+                });
+        }
     });
 })(window, rJS);
